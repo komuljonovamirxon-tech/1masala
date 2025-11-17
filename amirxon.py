e81b99f1551ffdbc532f59fc2027fe230af859ac
Amirxon <komuljonovamirxon@gmail.com>
2025-11-17 18:08:27 +0500

    Comment out existing code in amirxon.py
    Comment out the existing code for name and greetings.

--- amirxon.py
+++ amirxon.py
@@ -1,3 +1,28 @@
-ism = " maqsadbek"
-for x in range(1, len(ism)):
-    print(f"{x}: {ism[x]}")
+# ism = " maqsadbek"
+# for x in range(1, len(ism)):
+#     print(f"{x}: {ism[x]}")
+
+# dost = ("davlat","ozod", "murod","odom", "davlat2", "bekki","roma","asad","ibo","hasan")
+# for salom in dost:
+#     if salom == "davlat":
+#         print("hi",salom)
+#     elif salom == "ozod":
+#         print("hello",salom)
+#     elif salom == "murod":
+#         print("salom",salom)
+#     elif salom == "odom":
+#         print("assalom",salom)
+#     elif salom == "davlat2":
+#         print("privet",salom)
+#     elif salom == "bekki":
+#         print("nichiksan",salom)
+#     elif salom == "roma":
+#         print("anixasiyo",salom)
+#     elif salom == "asad":
+#         print("zdrastvuyte",salom)
+#     elif salom == "ibo":
+#         print("ohayo",salom)
+#     elif salom == "hasan":
+#         print("sammakim",salom)
+#     else:
+#         print("notanish")
